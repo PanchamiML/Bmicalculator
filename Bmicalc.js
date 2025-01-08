24fd489c433b5ad63a4366e0e44d5118c1615336
Panchami <unni09103@gmail.com>
2025-01-08 11:22:58 +0530

    want to check working

--- Bmicalc.js
+++ Bmicalc.js
@@ -4,14 +4,34 @@ const height = parseFloat(document.getElementById("height").value);
 const calculate=document.getElementById("calcbtn");
 const clear=document.getElementById("clrbtn");
 
-calculate.onclick =  displaybmi;
-function displaybmi(){
-//const display = document.getElementById("display");
-//const weight = parseFloat(document.getElementById("weight").value);
-//const height = parseFloat(document.getElementById("height").value);
-//const result  = (weight/(height*height)) * 10000;
-display.value = result;
 
 
-}
+// function displaybmi(){
+//     //const display = document.getElementById("display");
+//     //const weight = parseFloat(document.getElementById("weight").value);
+//     //const height = parseFloat(document.getElementById("height").value);
+//     const result  = (weight/(height*height)) * 10000;
+//     console.log(weight);
+//     console.log(height);
+//     //display.value = result;
+    
+    
+//     }
+
+function clearbmi(){
+        display.value="";
+        weight = 0;
+        height =0;
+    }
+
+
+
+
+
+calculate.addEventListener("click",()=>{
+    console.log("hello");
+})
+
+
+clear.onclick = clearbmi;
 
